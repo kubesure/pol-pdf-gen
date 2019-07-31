411f6501832957231ca25e2fd8844f82f7536f21
kubesure <edakghar@gmail.com>
2019-07-31 23:27:13 +0530

    PDF generation and put to processed folder

--- src/main/java/kubesure/io/pol/pdf/gen/PolicyPDFGeneratorHandler.java
+++ src/main/java/kubesure/io/pol/pdf/gen/PolicyPDFGeneratorHandler.java
@@ -1,6 +1,9 @@
 package kubesure.io.pol.pdf.gen;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import com.amazonaws.regions.Regions;
@@ -12,28 +15,32 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
 import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectResult;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
 public class PolicyPDFGeneratorHandler implements RequestHandler<S3Event, String> {
 
     @Override
     public String handleRequest(S3Event event, Context context) {
         LambdaLogger logger = context.getLogger();
+        String result = null;
         for (S3EventNotificationRecord record : event.getRecords()) {
-            
             try {
-                processEvent(record,logger);
+                result = processEvent(record, logger);
             } catch (Exception e) {
                 logger.log(e.getMessage());
                 e.printStackTrace();
                 return "error";
             }
         }
-        return "sucess";
+        return result;
     }
 
-    private void processEvent(S3EventNotificationRecord record, LambdaLogger logger) throws Exception {
+    private String processEvent(S3EventNotificationRecord record, LambdaLogger logger) throws Exception {
         AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
         String bucketName = record.getS3().getBucket().getName();
         String objectName = record.getS3().getObject().getKey();
@@ -46,17 +53,32 @@ public class PolicyPDFGeneratorHandler implements RequestHandler<S3Event, String
         BufferedReader reader = new BufferedReader(new InputStreamReader(s3stream));
         StringBuilder sbuilder = new StringBuilder();
         String htmlString = null;
-        while((htmlString = reader.readLine()) != null) {
-            //logger.log(htmlString + "\n");
+        while ((htmlString = reader.readLine()) != null) {
             sbuilder.append(htmlString);
         }
         s3stream.close();
-        generatePDF(sbuilder.toString(), logger);
+        byte [] pdf = generatePDF(sbuilder.toString(), logger);
+
+        InputStream in = new ByteArrayInputStream(pdf);
+        ObjectMetadata meta = new ObjectMetadata();
+        meta.setContentType("application/pdf");
+        meta.setContentLength(pdf.length);
+        String polnumber = objectName.substring(29, objectName.indexOf("."));
+        PutObjectResult result = s3.putObject(bucketName, "processed/"+ polnumber+".pdf", in, meta);
+        in.close();
+        return result.getVersionId();
     }
 
-    private void generatePDF(String htmlString,LambdaLogger logger) {
+    public byte [] generatePDF(String htmlString, LambdaLogger logger) throws Exception {
         logger.log("htmlString" + "\n");
         logger.log(htmlString);
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.setDocumentFromString(htmlString);
+        renderer.layout();
+        try(ByteArrayOutputStream bos = new ByteArrayOutputStream(htmlString.length())) {
+            renderer.createPDF(bos);
+            //logger.log(bos.toByteArray());
+            return bos.toByteArray();
+        }
     }
-
 }
--- src/test/java/pol/pdf/gen/PolicyPDFGeneratorTest.java
+++ src/test/java/pol/pdf/gen/PolicyPDFGeneratorTest.java
@@ -4,12 +4,54 @@
 package pol.pdf.gen;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URL;
+
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
+import kubesure.io.pol.pdf.gen.PolicyPDFGeneratorHandler;
+
 public class PolicyPDFGeneratorTest {
-    @Test public void testSomeLibraryMethod() {
-        //PolicyPDFGeneratorHandler pg = new PolicyPDFGeneratorHandler();
-        assertEquals("someLibraryMethod should return 'true'", "someLibraryMethod should return 'true'");
+    @Test public void testCreatePDF() throws Exception{
+        LambdaLogger logger = new LambdaLogger(){
+            @Override
+            public void log(byte[] message) {
+                System.out.println(message.toString());
+            }
+        
+            @Override
+            public void log(String message) {
+                System.out.println(message);
+            }
+        };
+       
+        URL url = getClass().getResource("unprocessed_html_1234567890.html");
+        File file = new File(url.getPath());
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        StringBuilder sbuilder = new StringBuilder();
+        String line; 
+        while((line = reader.readLine()) != null) {
+            sbuilder.append(line);
+        } 
+        reader.close();
+        PolicyPDFGeneratorHandler pg = new PolicyPDFGeneratorHandler();
+        byte [] pdfbytes = pg.generatePDF(sbuilder.toString(),logger);
+        File pdf = new File("1234567890.pdf");
+        FileUtils.writeByteArrayToFile(pdf, pdfbytes);
+        assertTrue("pdf file not generated", pdf.exists());
+    }
+
+    @Test
+    public void testFileSubString() {
+        String objectKey = "unprocessed/unprocessed_html_1234567890.html";
+        String polnumber = objectKey.substring(29, objectKey.indexOf("."));
+        assertEquals("1234567890", polnumber);
     }
 }
